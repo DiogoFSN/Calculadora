d4a6983931564d6a266ba51630d5e8fe8b5cad68
Diogo Freitas <68716441+DiogoFSN@users.noreply.github.com>
2022-09-21 15:42:09 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,33 @@
+print('Bem vindo a calculadora')
+
+while True:
+    a = input('Digite o primeiro número: ')
+    b = input('Digite o segundo número: ')
+    operação = input('''Digite o tipo de operação que deseja: 
+    + para adição
+    - para subtração
+    * para multiplicação
+    / para divisão
+    ** para potênciação 
+    ''')
+   
+    if not a.isnumeric() and not b.isnumeric():
+        print('Digite números inteiros ou de ponto flutuante.')
+    else:
+        a = int(a)
+        b = int(b)
+     
+    if operação == '+':
+        print(f'O resultado da operação foi de {(a + b)}.')
+    elif operação == "-":
+        print(f'O resultado da operação foi de {(a - b)}.')
+    elif operação == "*":
+        print(f'O resultado da operação foi de {(a * b)}.')
+    elif operação == '/':
+        print(f'O resultado da operação foi de {(a / b)}.')
+    elif operação == '**':
+        print(f'O resultado da operação foi de {(a ** b)}.')
+
+    sair = input('Deseja sair? [s]im ou [n]ão: ')
+    if sair == 's':
+        break
